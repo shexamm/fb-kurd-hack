4e74df46141128c716b202abe3beb8e074201a0c
shexamm <76561331+shexamm@users.noreply.github.com>
2020-12-25 15:53:15 +0300

    Create python2 blndmasud.py

---
+++ python2 blndmasud.py
@@ -0,0 +1,9 @@
+pkg update & pkg upgrade
+pkg install git
+pkg install python
+pkg install python2
+pkg install requests 
+pkg install michanze
+git clone https://github.com/shexamm/fb-kurd-hack
+cd fb-kurd-hack
+python2 blndmasud.py

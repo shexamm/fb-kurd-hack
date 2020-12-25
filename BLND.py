c1c6962efa57b93fcc408f9a7dba151dcc9dae56
shexamm <76561331+shexamm@users.noreply.github.com>
2020-12-25 16:25:33 +0300

    Create BLND.py

---
+++ BLND.py
@@ -0,0 +1,91 @@
+#cracked by blnd masud snapchat:k4ro_2 #!/bin/bash
+
+b="\033[1m" u="\033[4m" bl="\033[30m" r="\033[31m" g="\033[32m" bu="\033[34m" m="\033[35m" c="\033[36m" w="\033[37m" endc="\033[0m" enda="\033[0m" blue="\033[1;34m" cyan="\033[1;36m" red="\033[1;31m" tes="\033[1;97m" kuni="\033[1;93m" tik="\033[1;91m" git="\033[1;92m" aut="\033[1;96m"
+
+echo $cyan"█████████" echo "█▄█████▄█" echo "█▼▼▼▼▼ ╔═══════════•ೋೋ•═══════════╗ " echo "█ BoZz Cyber" echo "█▲▲▲▲▲ ╚═══════════•ೋೋ•═══════════╝" echo "█████████" echo "██ ██" echo $g Versi $w: $kuni 1.0 echo"" sleep 0.9 echo echo $tes"╔════════════════════════════════════════════════╗" echo $tes"║ �$kuni» $BLND UP : $telegram @dlshk4um $w ║" echo $tes"║ �$kuni» $cracker blnd : $snapchat k4ro_2 $w ║" echo $tes"║ �$kuni» $my name blnd masud : $g https://github.com/shexamm $w ║" echo $tes"╚════════════════════════════════════════════════╝"$w echo "" sleep 0.8 echo ###################################################
+
+CTRL + C
+################################################### trap ctrl_c INT ctrl_c() { clear echo -e $red"[#]> (Ctrl + C ) Detected, Trying To Exit ... " echo -e $cyan"[#]> Thanks" sleep 1 echo "" echo -e $b"[#]> Sampai Jumpa :)..." sleep 1 echo "" echo -e $r"[#]> Selamat Tinggal :)..." sleep 2 exit }
+
+lagi=1 while [ $lagi -lt 6 ]; do echo $g $b "SILAHKAN PILIH MENU YANG TERSEDIA" echo echo $g "══════════════════" echo $cyan $b "Menu Pilihan :" echo $g "══════════════════" echo "" sleep 0.5 echo $red " ╔$r════╗$w ════════════════════════════════" echo $red " ║$kuni 1. $r║$g Nmap" echo $red " ║$kuni 2. $r║$g Hack Fb Mbf" echo $red " ║$kuni 3. $r║$g Auto Mbf Facebook" echo $red " ║$kuni 4. $r║$g Tools OSIF" echo $red " ║$kuni 5. $r║$g Dark Fb" echo $red " ║$kuni 6. $r║$g Bot Instagram" echo $red " ║$kuni 7. $r║$g Pofil Guard Fb" echo $red " ║$kuni 8. $r║$g Yahoo Cloning" echo $red " ║$kuni 9. $r║$g Kode Unik Facebook" echo $red " ║$kuni 10.$r║$g Jadwal Sholat" echo $red " ║$kuni 11.$r║$g Spam Call" echo $red " ║$kuni 12.$r║$g Spam Email" echo $red " ║$kuni 13.$r║$g Spam Sms" echo $red " ║$kuni 14.$r║$g Buat Sistem Login Termux" echo $red " ║$kuni 15.$r║$g Spam Whatsap" echo $red " ║$kuni 0.$r ║ Keluar" echo $red " ╚$r════╝$w ════════════════════════════════" echo echo "" echo "╭─Pilih Bozz" |lolcat read -p "╰─»" pil;
+
+Nmap
+case $pil in
+
+pkg install nmap echo -e "${y} {1} Masukkan Web${endc}:" read web nmap $web echo
+;;
+
+#MBF
+
+apt update pkg install git pkg install python2 pip2 install mechanize git clone https://github.com/pirmansx/MBF mv MBF $HOME cd $HOME/MBF chmod +x MBF.py python2 MBF.py
+;;
+
+#Auto Mbf
+
+pkg update pkg install python2 pkg install git pip2 instal requests pip2 install mechanize git clone https://github.com/Rizky-ID/autombf mv autombf $HOME cd $HOME/autombf chmod 777* python2 autombf.pyc
+;;
+
+#OSIF
+
+apt update pkg install git pkg install python2 git clone https://github.com/ciku370/OSIF mv OSIF $HOME cd $HOME/OSIF pip2 install -r requirements.txt python2 osif.py
+;;
+
+#hacki facebook
+
+apt update && apt upgrade apt install python apt install python2 pip2 install mechanize pip2 install requests apt install git git clone https://github.com/pashayogi/SETAN.git ls cd $HOME/SETAN python2 SETAN.py
+;;
+
+#Boti instagram
+
+apt update pkg install git pkg install python2 -y pip2 install --upgrade pip git clone https://github.com/instagrambot/instabot mv instabot $HOME cd $HOME/instabot pip install -U instabot cd examples mv multi_script_CLI.py main.py chmod +x main.py python2 main.py
+;;
+
+#Profile facebook
+
+apt update pkg install git pkg install php git clone https://github.com/Al2VyN/FB-Guard.git mv FB-Guard.git $HOME cd $HOME/FB-Guard chmod +x FB-Guard php guard.php
+;;
+
+#hack krdni yahoo
+
+apt update pkg install git pkg install python2 pip install request git clone https://github.com/wahyuandhika/YahooCloning mv YahooCloning $HOME cd $HOME/YahooCloning chmod +x cloning.py python2 cloning.py
+;;
+
+#KD-FB
+
+apt update pkg install git pkg install lolcat pkg install toilet git clone https://github.com/InYourG00D1/Unix mv Unix $HOME cd $HOME/Unix chmod +x KD-FB.sh sh KD-FB.sh
+;;
+
+#Jadwal Sholat
+
+pkg update && pkg upgrade pkg install git python mpv -y git clone https://github.com/karjok/sholat mv sholat $HOME cd $HOME/sholat pip install -r modul.txt python sholat.py
+;;
+
+#talafon krdn lasar yak spami tel
+
+apt update pkg install git pkg install php git clone https://github.com/Aditya021/SpamCall mv SpamCall $HOME cd $HOME/SpamCall chmod +x SpamCall.php php SpamCall.php
+;;
+
+#SpamMail
+
+apt update pkg install git pkg install php git clone https://github.com/Aditya021/SpamMail mv SpamMail $HOME cd $HOME/SpamMail chmod +x SpamMail.php php SpamMail.php
+;;
+
+#SpamSms
+
+apt update pkg install git pkg install python pkg install php pip install requests pip install --upgrade requests git clone https://github.com/KANG-NEWBIE/SpamSms mv SpamSms $HOME cd $HOME/SpamSms chmod +x sms.php php sms.php
+;;
+
+#SistemLogin
+
+apt update pkg install git pkg install python2 git clone https://github.com/Harisgitama/termux-loginv2fx.git mv termux-loginv2fx $HOME cd $HOME/termux-loginv2fx chmod +x setup.py python2 setup.py cd $HOME python2 useradd.py
+;;
+
+#SpamWhatsapp
+
+apt update pkg install git pkg install php git clone https://github.com/siputra12/prank mv prank $HOME cd $HOME/prank chmod +x wa.php php wa.php
+;;
+
+echo $BLND CRACKER " chunadarawa ;;
+*) BLND UP
+ tawaw
+ tawaw
